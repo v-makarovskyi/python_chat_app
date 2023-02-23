0bc57d753e4376d8a0103c4b931a2c41fb4b55de
Vladimir <bigcorvet@gmail.com>
2023-02-23 10:38:41 +0200

    add __init__()

---
+++ client.py
@@ -0,0 +1,18 @@
+from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, Button, messagebox
+import socket
+import threading
+
+class GUI:
+    client_socket = None
+    last_received_message = None
+
+    def __init__(self, master):
+        self.root = master
+        self.chat_transcript_area = None
+        self.name_widget = None
+        self.enter_text_widget = None
+        self.join_button = None
+        self.initialize_socket()
+        self.initialize_gui()
+        self.listen_incoming_messages_in_a_thread()
+         
