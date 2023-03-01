88e0349619f8b8bfdec54ad5a1653989c1af6dff
Vladimir <bigcorvet@gmail.com>
2023-03-01 12:43:41 +0200

    add def initialize GUI

--- client.py
+++ client.py
@@ -17,7 +17,18 @@ class GUI:
         self.listen_incoming_messages_in_a_thread()
 
     def initialize_socket(self):
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        """ инициализация и подключение сокета с TCP и IPv4 к удаленному серверу """
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
         remote_ip = '127.0.0.1'
         remote_port = 10319
-        self.client_socket.connect((remote_ip, remote_port))
+        self.client_socket.connect((remote_ip, remote_port)) #подключение к удаленному серверу
+
+
+    def initialize_gui(self): 
+        """ инициализация графического интерфейса пользователя """
+        self.root.title('Python-Socket-Chat-App')
+        self.root.resizable(0,0)
+        self.display_chat_box()
+        self.display_name_section()
+        self.display_chat_entry_box()
+        
