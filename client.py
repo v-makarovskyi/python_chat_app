db327f796164b33aedf974f3df63a56339dc884e
Vladimir <bigcorvet@gmail.com>
2023-03-01 12:53:14 +0200

    create thread

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ from tkinter import Tk, Frame, Scrollbar, Label, END, Entry, Text, VERTICAL, But
 import socket
 import threading
 
+
 class GUI:
     client_socket = None
     last_received_message = None
@@ -18,17 +19,23 @@ class GUI:
 
     def initialize_socket(self):
         """ инициализация и подключение сокета с TCP и IPv4 к удаленному серверу """
-        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
+        self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         remote_ip = '127.0.0.1'
         remote_port = 10319
-        self.client_socket.connect((remote_ip, remote_port)) #подключение к удаленному серверу
+        # подключение к удаленному серверу
+        self.client_socket.connect((remote_ip, remote_port))
 
-
-    def initialize_gui(self): 
-        """ инициализация графического интерфейса пользователя """
+    def initialize_gui(self):
+        """ инициализация графического интерфейса пользователя. Прослушивание входящих сообщений в потоке """
         self.root.title('Python-Socket-Chat-App')
-        self.root.resizable(0,0)
+        self.root.resizable(0, 0)
         self.display_chat_box()
         self.display_name_section()
         self.display_chat_entry_box()
+
+    def listen_for_incoming_messages_in_a_thread(self):
+        """ Создание потока для одновременной отправки и получения сообщений """
+        thread = threading.Thread(
+            target=self.receive_message_from_server, args=(self.client_socket,))
+        thread.start()
         
